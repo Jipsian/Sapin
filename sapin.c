534308a657c8f7b5efef9b3aeea9b716ec5c83a3
Gecko <labarre.sylvain@coding-academy.fr>
2019-02-15 17:32:53 +0100

    histoire de

--- sapin.c
+++ sapin.c
@@ -11,7 +11,7 @@ void put_space(int n)
   int i = 0;
   while (i < n)
   {
-   my_putchar(' ');
+   my_putchar('0');
    i++;
   }
 }
@@ -36,9 +36,27 @@ void new_line(int n)
   }
 }
 
-void sapin_level1()
+int nb_lignes(int size)
 {
-  int space = 3;
+  int start = 4;
+  if (size > 1)
+    start = start + size;
+  return start;
+}
+
+
+int stars_base(int size)
+{
+  int start = 7;
+  int residu = 6;
+  int count = 1;
+  
+  start = start + 8;
+}
+
+void sapin_level1(size)
+{
+  int space = 3; 
   int star = 1;
   int count = 4;
   
@@ -53,19 +71,40 @@ void sapin_level1()
   }
 }
 
+void sapin_leveln(int start)
+{
+  int space, star, stair;
+  space = 0;
+  stair = start + 3;
+  star = (stair * start) - 1; 
+  while (start != 1)
+    {
+      put_space(space);
+      put_star(star);
+      new_line(1);
+      star = star - 2;
+      space++;
+      start--;
+      sapin_leveln(start);
+    }
+}
+
 void sapin(int size)
 {
   if (size == 1)
     sapin_level1();
   else
-    my_putchar('1');
+    {
+      sapin_level1();
+      sapin_leveln(size);
+    }
 }
 
 int main(int argc, char **argv)
 {
   int nb;
   
-  sapin(1);
+  sapin(3);
   
   nb = atoi(argv[1]);
 
